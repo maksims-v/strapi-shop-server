55ad5aaa84afc6bb03f84fcab5e8116cd4b46f3b
maksims-v <spread2010@inbox.ru>
2024-10-18 16:31:24 +0300

    ssl fixes

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,7 +1,9 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password, ssl } = parse(
+    env("DATABASE_URL")
+  );
   return {
     connection: {
       client: "postgres",
@@ -11,6 +13,9 @@ module.exports = ({ env }) => {
         database,
         user,
         password,
+        ssl: {
+          rejectUnauthorized: false, // или true, в зависимости от требований
+        },
       },
       debug: false,
     },
